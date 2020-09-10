83b6204d265810ca1d0ebfb5dfd99ece304e3b5f
Raul Quintanilla <oolieq@gmail.com>
2020-09-09 21:46:11 -0500

    feat(#4): test

--- packages/store/src/functions/product/index.js
+++ packages/store/src/functions/product/index.js
@@ -1,8 +1,10 @@
 exports.handler = (event, context, callback) => {
   // eslint-disable-next-line no-console
-  console.log(JSON.stringify(event));
+  // console.log(`event : \n${JSON.stringify(event, null, 2)}`);
   // eslint-disable-next-line no-console
-  console.log(JSON.stringify(context));
+  console.log(`body : \n${JSON.stringify(event.body, null, 2)}`);
+  // eslint-disable-next-line no-console
+  // console.log(JSON.stringify(context));
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
