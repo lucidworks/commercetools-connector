59b67b26bd7db6a3c93d6e9a5cc24ae904bbdc51
Raul Quintanilla <oolieq@gmail.com>
2020-09-09 22:44:07 -0500

    test: payload enc

--- packages/store/src/functions/product/index.js
+++ packages/store/src/functions/product/index.js
@@ -4,7 +4,7 @@ exports.handler = (event, context, callback) => {
   const message = JSON.parse(event.body, null, 2);
   // eslint-disable-next-line no-console
   console.log(`message : \n${JSON.stringify(message, null, 2)}`);
-  const value = Buffer.from(message.message.data, 'base64').toString();
+  const value = JSON.parse(Buffer.from(message.message.data, 'base64').toString());
   // eslint-disable-next-line no-console
   console.log(`value : \n${JSON.stringify(value, null, 2)}`);
   // eslint-disable-next-line no-console
