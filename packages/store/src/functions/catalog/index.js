66d01a86288afe742e1fb36ac70a7f112fbeb242
Raul Quintanilla <raul.quintanilla@newelevation.com>
2020-09-28 09:10:34 -0500

    feat: adding baseline function

--- packages/store/src/functions/catalog/index.js
+++ packages/store/src/functions/catalog/index.js
@@ -6,11 +6,12 @@ exports.handler = async (event, context, callback) => {
   const value = JSON.parse(Buffer.from(message.message.data, 'base64').toString());
   console.log(`value : \n${JSON.stringify(value, null, 2)}`);
 
+  
   try {
-    const res = await Index.create([value]);
-    console.log(`res : \n${JSON.stringify(res, null, 2)}`);
+    const res = await Index.create(value);
+    console.log(`Index Updated \n${JSON.stringify(res, null, 2)}`);
   } catch (error) {
-    console.error('Could not create', error);
+    console.error('Could not complete import baseline', error);
   }
 
   callback(null, {
