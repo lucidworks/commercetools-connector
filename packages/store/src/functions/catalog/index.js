dbf716ae7ab911db4edd4339f7770f9d60a0fa8b
Raul Quintanilla <raul.quintanilla@newelevation.com>
2020-09-29 08:51:38 -0500

    feat: baseline cleanup before merge

--- packages/store/src/functions/catalog/index.js
+++ packages/store/src/functions/catalog/index.js
@@ -3,12 +3,12 @@ const { Index } = require('fusion-sdk');
 
 exports.handler = async (event, context, callback) => {
   const message = JSON.parse(event.body, null, 2);
-  const value = JSON.parse(Buffer.from(message.message.data, 'base64').toString());
-  console.log(`value : \n${JSON.stringify(value, null, 2)}`);
+  // const value = JSON.parse(Buffer.from(message.message.data, 'base64').toString());
+  console.log(`Message : \n${JSON.stringify(message, null, 2)}`);
 
-  
+
   try {
-    const res = await Index.create(value);
+    const res = await Index.create(message);
     console.log(`Index Updated \n${JSON.stringify(res, null, 2)}`);
   } catch (error) {
     console.error('Could not complete import baseline', error);
