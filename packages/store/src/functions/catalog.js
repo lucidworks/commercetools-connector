048b6fc3409b081e4729ee709f530051adf8dfcc
Raul Quintanilla <raul.quintanilla@newelevation.com>
2020-09-29 09:48:50 -0500

    chore: import fix

--- packages/store/src/functions/catalog.js
+++ packages/store/src/functions/catalog.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-console */
-const { Index } = require('fusion-sdk');
+const Index = require('fusion-sdk');
 
 exports.handler = async (event, context, callback) => {
   const message = JSON.parse(event.body, null, 2);
   // const value = JSON.parse(Buffer.from(message.message.data, 'base64').toString());
   console.log(`Message : \n${JSON.stringify(message, null, 2)}`);
 
-  const api = new Index();
   try {
+    const api = new Index();
     const res = await api.create(message);
     console.log(`Baseline Complete \n${JSON.stringify(res, null, 2)}`);
   } catch (error) {
